3b8ba01775bc69a1a7f051eb899ee5825bbf2ef9
Chenzhao Huang <949412843@qq.com>
2023-09-27 00:09:57 -0400

    fix a bug in tss, but another bug occurs.

--- kern/env.c
+++ kern/env.c
@@ -534,6 +534,7 @@ env_run(struct Env *e)
 	curenv=e;
 	curenv->env_status=ENV_RUNNING;
 	curenv->env_runs++;
+	cprintf("????????%d\n%d\n",cpunum(),ENVX(curenv->env_id));
 	lcr3(PADDR(curenv->env_pgdir));
 	unlock_kernel();
 	env_pop_tf(&(curenv->env_tf));
